44e8c7a88e5eae1539a1cb7a30be95fa5ba4e070
shahsaaqib01 <shahsaaqib01@gmail.com>
2021-02-15 10:06:51 +0530

    addRemoved

--- first1.js
+++ first1.js
@@ -3,8 +3,8 @@ const num2 = 3;
 
 // sub two numbers
 const sub = num1 - num2;
-const sum = num1 + num2;
+//const sum = num1 + num2;
 
 // display the sub
 console.log('The subtraction of ' + num1 + ' and ' + num2 + ' is: ' + sub);
-console.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum);
+//console.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum);
