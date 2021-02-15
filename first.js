261a30207621614e824499c7943a7484068fdafa
shahsaaqib01 <shahsaaqib01@gmail.com>
2021-02-15 08:45:06 +0530

    lastCommit

---
+++ first.js
@@ -0,0 +1,10 @@
+const num1 = 5;
+const num2 = 3;
+
+// add two numbers
+const sum = num1 + num2;
+const mul = num1 * num2;
+
+// display the sum
+console.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum);
+console.log('The product of ' + num1 + ' and ' + num2 + ' is: ' + mul);
---
+++ first1.js
@@ -0,0 +1,10 @@
+const num1 = 5;
+const num2 = 3;
+
+// sub two numbers
+const sub = num1 - num2;
+const sum = num1 + num2;
+
+// display the sub
+console.log('The subtraction of ' + num1 + ' and ' + num2 + ' is: ' + sub);
+console.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum);
