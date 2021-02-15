cfb98ae15b56eeeded12de3fcdb3d9e814e67f7c
shahsaaqib01 <shahsaaqib01@gmail.com>
2021-02-15 10:08:38 +0530

    productRemoved

--- first.js
+++ first.js
@@ -3,8 +3,8 @@ const num2 = 3;
 
 // add two numbers
 const sum = num1 + num2;
-const mul = num1 * num2;
+//const mul = num1 * num2;
 
 // display the sum
 console.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum);
-console.log('The product of ' + num1 + ' and ' + num2 + ' is: ' + mul);
+//console.log('The product of ' + num1 + ' and ' + num2 + ' is: ' + mul);
